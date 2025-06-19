dfeed719133c125d09e81099f292a3aa040a4a83
omkar shirsath <omkarshirsath28@gmail.com>
2025-06-20 00:19:09 +0530

    index page styling complete

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
 
+
 const MONGO_URL = "mongodb://127.0.0.1:27017/Nestago";
 async function main(){
     await mongoose.connect(MONGO_URL);
@@ -40,8 +41,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"public")));
+app.engine('ejs',ejsMate);
+app.use(express.static (path.join(__dirname,"public")));
 
 //index rought
 app.get("/listings",async(req,res)=>{
